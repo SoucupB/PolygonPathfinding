454734a0a0309380a336e513181b1841a0edeb04
SoucupB <bogdan.soucup@gmail.com>
2024-01-27 23:28:20 +0200

    Add index exporter

---
+++ package/index.js
@@ -0,0 +1,5 @@
+import Polygon from './sources/Geometry/Polygon.js'
+import Point from './sources/Geometry/Point.js';
+import Search from './sources/Searcher/Search.js';
+
+export { Polygon, Point, Search };
